1f55bcb4ff28f1a9d3e10dda11716797559ba36d
iamyewon <rla357987@gmail.com>
2024-09-13 09:32:38 +0900

    03-reviews
    03-reviews

--- 02-Counter/app.js
+++ 02-Counter/app.js
@@ -2,10 +2,21 @@ const value = document.querySelector('#value');
 const decrease = document.querySelector('.decrease');
 const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
+// 많을 때 반복문으로 / 배열 하나 만들어서.
+
+// const changeColor = () => { // 하는 일 따라 함수 분리
+//     if(value.textContent === '0'){
+//         value.style.color = '#102A42';
+//     }else if(value.textContent > 0){
+//         value.style.color = 'green';
+//     }else{
+//         value.style.color = 'red';
+//     }
+// }
 
 const updateValue = (amount) => {
     value.textContent = Number(value.textContent) + amount;
-    
+
     if(value.textContent === '0'){
         value.style.color = '#102A42';
     }else if(value.textContent > 0){
@@ -15,7 +26,12 @@ const updateValue = (amount) => {
     }
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+// const handle = () => {
+//     updateValue();
+//     changeColor();
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
     decrease.addEventListener('click', () => updateValue(-1));
     increase.addEventListener('click', () => updateValue(1));
     reset.addEventListener('click', () => updateValue(-Number(value.textContent)));
